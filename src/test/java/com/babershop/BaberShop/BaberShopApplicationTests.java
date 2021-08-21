f2f20bcbb8de47d5df406c1cb2d825a40cd3abc6
JacksonAGomes <jacksonanderson1515@hotmail.com>
2021-08-21 09:42:14 -0300

    initial commit

---
+++ src/test/java/com/babershop/BaberShop/BaberShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babershop.BaberShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaberShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
