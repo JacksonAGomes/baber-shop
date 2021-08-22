365c6ed879865c8fa35c5b73386602517113cc66
JacksonAGomes <jacksonanderson1515@hotmail.com>
2021-08-22 10:23:08 -0300

    implemented methods in CorteService

--- src/main/java/com/babershop/BaberShop/service/CorteService.java
+++ src/main/java/com/babershop/BaberShop/service/CorteService.java
@@ -1,12 +1,33 @@
 package com.babershop.BaberShop.service;
 
+import com.babershop.BaberShop.exception.EntityNotFoundException;
+import com.babershop.BaberShop.model.Corte;
 import com.babershop.BaberShop.repository.CorteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CorteService {
 
     @Autowired
     private CorteRepository corteRepository;
+
+    public Corte salvar(Corte corte){
+        return corteRepository.save(corte);
+    }
+
+    public List<Corte> listar(){
+        return corteRepository.findAll();
+    }
+
+    public Corte buscar(Long corteId){
+        return corteRepository.findById(corteId).orElseThrow(EntityNotFoundException::new);
+    }
+
+    public void deletar(Long corteId){
+        Corte corte = this.buscar(corteId);
+        corteRepository.delete(corte);
+    }
 }
