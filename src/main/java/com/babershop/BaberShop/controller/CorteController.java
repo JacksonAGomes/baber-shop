f2c1cf55d7f1a9f4ffa80f8f114d643b5fd0aad7
JacksonAGomes <jacksonanderson1515@hotmail.com>
2021-08-22 10:47:46 -0300

    end points and methods in CorteController implementeds

--- src/main/java/com/babershop/BaberShop/controller/CorteController.java
+++ src/main/java/com/babershop/BaberShop/controller/CorteController.java
@@ -1,9 +1,16 @@
 package com.babershop.BaberShop.controller;
 
+import com.babershop.BaberShop.exception.EntityNotFoundException;
+import com.babershop.BaberShop.model.Corte;
 import com.babershop.BaberShop.service.CorteService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cortes")
@@ -11,4 +18,49 @@ public class CorteController {
 
     @Autowired
     private CorteService corteService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Corte salvar(@RequestBody Corte corte){
+        return corteService.salvar(corte);
+    }
+
+    @GetMapping
+    public List<Corte> listar(){
+        return corteService.listar();
+    }
+
+    @GetMapping("/{corteId}")
+    public ResponseEntity<Corte> buscar(@PathVariable Long corteId){
+        try{
+            Corte corte = corteService.buscar(corteId);
+            return ResponseEntity.ok(corte);
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{corteId}")
+    public ResponseEntity<Corte> atualizar(@PathVariable Long corteId, @RequestBody Corte corte){
+        try {
+            Corte corteAtual = corteService.buscar(corteId);
+            BeanUtils.copyProperties(corte, corteAtual, "id");
+            return ResponseEntity.ok(corteAtual);
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{corteId}")
+    public ResponseEntity<?> deletar(@PathVariable Long corteId){
+        try{
+            corteService.deletar(corteId);
+            return ResponseEntity.noContent().build();
+
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }catch (DataIntegrityViolationException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("entity in use, cannot delete ");
+        }
+    }
 }
