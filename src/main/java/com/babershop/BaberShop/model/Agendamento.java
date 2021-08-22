920947c46e34c4ade7dcb7b157288dc9c35b2452
JacksonAGomes <jacksonanderson1515@hotmail.com>
2021-08-22 11:00:53 -0300

    entity Agentamento created

---
+++ src/main/java/com/babershop/BaberShop/model/Agendamento.java
@@ -0,0 +1,27 @@
+package com.babershop.BaberShop.model;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Agendamento {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @ManyToOne
+    private Cliente cliente;
+    
+    @ManyToOne
+    private Corte corte;
+
+    @CreationTimestamp
+    private LocalDateTime dataDoAgentamento;
+
+    private LocalDateTime horaMarcada;
+}
